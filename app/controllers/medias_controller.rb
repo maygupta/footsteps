99a783efedc2c8270956a32afbe9cf8a44f226e1
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-08-31 16:33:45 +0530

    added filter in media

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,7 +1,11 @@
 class MediasController < ApplicationController
 
   def index
-    render :json => Medium.all, :status => 200
+    if params[:category].present?
+      render :json => Medium.where(:category => params[:category]), :status => 200
+    else
+      render :json => Medium.all, :status => 200
+    end
   end
 
 end
