42e5f82b4ea164ad18736afcaf9bb340978d99a6
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-12-14 15:23:24 -0800

    Load annoucnements in cors

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,10 @@
 class AnnouncementsController < ApplicationController
 
   def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     render :json => Announcement.order(id: :desc).all, :status => 200
   end
 
