d004955e5e59f5a24a41256d5269922327ddeb76
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-12-14 15:16:45 -0800

    added headers for media

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,6 +1,10 @@
 class MediasController < ApplicationController
 
   def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     if params[:category].present?
       render :json => Medium.where(:category => params[:category]).order(id: :desc), :status => 200
     else
