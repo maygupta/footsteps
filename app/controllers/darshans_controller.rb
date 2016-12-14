2c606847128c97e70e5914a6a66d5ebfff015619
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-12-14 15:10:23 -0800

    allow cors

--- app/controllers/darshans_controller.rb
+++ app/controllers/darshans_controller.rb
@@ -1,6 +1,10 @@
 class DarshansController < ApplicationController
 
   def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     render :json => Darshan.order(id: :desc).all, :status => 200
   end
 
