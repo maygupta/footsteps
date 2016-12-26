aa150bade0ab82485744dceba7a6c91c02e6a199
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2016-12-26 12:10:43 -0800

    Return only top 10 darshans

--- app/controllers/darshans_controller.rb
+++ app/controllers/darshans_controller.rb
@@ -5,7 +5,7 @@ class DarshansController < ApplicationController
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    render :json => Darshan.order(id: :desc).all, :status => 200
+    render :json => Darshan.order(id: :desc).all[0,10], :status => 200
   end
 
 end
