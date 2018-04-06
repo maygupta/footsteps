988d1d52aac6831dda5edc1fd39814c224bc8c4c
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2018-04-06 11:04:41 -0700

    added admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,6 +84,52 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+    @user = User.find(params[:id])
+
+    @total_rounds = 0
+    @total_reading_hours = 0
+    @total_hearing_hours = 0
+    @total_service_hours = 0
+    @total_sadhna_cards = 0
+
+    @current_month_rounds = 0
+    @current_month_reading_hours = 0
+    @current_month_hearing_hours = 0
+    @current_month_service_hours = 0
+    @current_month_sadhna_cards = 0
+
+    @month_name = Date.today.strftime("%B")
+    @month = Date.today.strftime("%m")
+    @year =  Date.today.strftime("%Y")
+
+    sadhna_cards = @user.sadhna_cards
+    current_month_sadhna_cards = @user.sadhna_cards.where('extract(year from date) = ? AND extract(month  from date) = ? 
+      ', @year, @month)
+
+    sadhna_cards.each do |sc|
+      @total_rounds += sc.japa_rounds
+      @total_service_hours +=  if sc.service.present? then sc.service.to_i else 0 end
+      @total_hearing_hours += if sc.hearing.present? then sc.hearing.to_i else 0 end
+      @total_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+    end
+    @total_service_hours = (@total_service_hours/60).to_s + "h " +  (@total_service_hours % 60).to_s + "m"
+    @total_reading_hours = (@total_reading_hours/60).to_s + "h " +  (@total_reading_hours % 60).to_s + "m"
+    @total_hearing_hours = (@total_hearing_hours/60).to_s + "h " +  (@total_hearing_hours % 60).to_s + "m"
+    @total_sadhna_cards = sadhna_cards.count
+
+    current_month_sadhna_cards.each do |sc|
+      @current_month_rounds += sc.japa_rounds
+      @current_month_service_hours +=  if sc.service.present? then sc.service.to_i else 0 end
+      @current_month_hearing_hours += if sc.hearing.present? then sc.hearing.to_i else 0 end
+      @current_month_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+    end
+    @current_month_service_hours = (@current_month_service_hours/60).to_s + "h " +  (@current_month_service_hours % 60).to_s + "m"
+    @current_month_reading_hours = (@current_month_reading_hours/60).to_s + "h " +  (@current_month_reading_hours % 60).to_s + "m"
+    @current_month_hearing_hours = (@current_month_hearing_hours/60).to_s + "h " +  (@current_month_hearing_hours % 60).to_s + "m"
+    @current_month_sadhna_cards = current_month_sadhna_cards.count
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
