faabf3c6e28bae555dda20292f3ae014f5f00a61
maygupta <nsit.mayankgupta@gmail.com>
2018-05-02 15:07:21 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -100,10 +100,12 @@ class UsersController < ApplicationController
     @total_reading_hours = 0
     @total_hearing_hours = 0
     @total_service_hours = 0
+    @total_service_pages = 0
     @total_sadhna_cards = 0
 
     @current_month_rounds = 0
     @current_month_reading_hours = 0
+    @current_month_reading_pages = 0   
     @current_month_hearing_hours = 0
     @current_month_service_hours = 0
     @current_month_sadhna_cards = 0
@@ -140,7 +142,11 @@ class UsersController < ApplicationController
       @total_rounds += sc.japa_rounds
       @total_service_hours +=  if sc.service.present? then sc.service.to_i else 0 end
       @total_hearing_hours += if sc.hearing.present? then sc.hearing.to_i else 0 end
-      @total_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+      if sc.reading_type == 'Mins'
+        @total_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+      else
+        @total_reading_pages += if sc.reading.present? then sc.reading.to_i else 0 end
+      end
     end
     @total_service_hours = (@total_service_hours/60).to_s + "h " +  (@total_service_hours % 60).to_s + "m"
     @total_reading_hours = (@total_reading_hours/60).to_s + "h " +  (@total_reading_hours % 60).to_s + "m"
@@ -151,7 +157,11 @@ class UsersController < ApplicationController
       @current_month_rounds += sc.japa_rounds
       @current_month_service_hours +=  if sc.service.present? then sc.service.to_i else 0 end
       @current_month_hearing_hours += if sc.hearing.present? then sc.hearing.to_i else 0 end
-      @current_month_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+      if sc.reading_type == 'Mins'
+        @current_month_reading_hours += if sc.reading.present? then sc.reading.to_i else 0 end
+      else
+        @current_month_reading_pages += if sc.reading.present? then sc.reading.to_i else 0 end
+      end
     end
     @current_month_service_hours = (@current_month_service_hours/60).to_s + "h " +  (@current_month_service_hours % 60).to_s + "m"
     @current_month_reading_hours = (@current_month_reading_hours/60).to_s + "h " +  (@current_month_reading_hours % 60).to_s + "m"
