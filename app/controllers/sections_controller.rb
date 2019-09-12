545c04609732d6bb3e06e9c80775d944ce40da72
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2019-09-11 21:39:42 -0700

    added sections controller

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,13 @@
+class SectionsController < ApplicationController
+  skip_before_filter :require_login
+  
+
+  def index
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    render :json => Section.all, :status => 200
+  end
+
+end
