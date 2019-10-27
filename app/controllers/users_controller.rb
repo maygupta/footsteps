87345aee8729a00f5e9fabed62421877e395e3b5
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2019-10-26 20:55:08 -0700

    fix login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_filter :verify_authenticity_token  
+  skip_before_filter :verify_authenticity_token
+  skip_before_filter :require_login
   
   include SadhnaCardHelper
 
