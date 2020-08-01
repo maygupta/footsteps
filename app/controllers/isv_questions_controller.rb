38bdc285f3c2c6dad502874cb635828f108f94dd
Mayank Gupta <mayank@Mayanks-MacBook-Air.local>
2020-08-01 13:13:43 -0700

    search api

--- app/controllers/isv_questions_controller.rb
+++ app/controllers/isv_questions_controller.rb
@@ -13,6 +13,20 @@ class IsvQuestionsController < ApplicationController
     render :json => IsvQuestion.all, :status => 200
   end
 
+  def search
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
+    if params[:query].present?
+      query = params[:query]
+      render :json => IsvQuestion.where("value LIKE ?", "%#{query}%"), :status => 200
+    else
+      render :json => [], :status => 200
+    end
+  end
+
   def import_csv
     csv_text = File.read('ask_reflect.csv')
     csv = CSV.parse(csv_text, :headers => true)
@@ -21,7 +35,8 @@ class IsvQuestionsController < ApplicationController
       ques = IsvQuestion.new(:ask_time => ask_time,
         :name => row[1],
         :ask_type => row[2],
-        :value => row[3])
+        :value => row[3],
+        :verse => row[4])
       ques.save!
     end
     render :json => csv, :status => 200
